53a191e567f485bf4c3c492ad170c7c2d9a5db44
Dominik <dominik.buettner1711@gmail.com>
2023-04-01 01:13:06 +0000

    Sorting

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ const SERVER_ID = "446997274132873220";
 const { WEBHOOK_URL, MEE6_TOKEN } = process.env;
 
 const months = [
-    "Januar",
-    "Februar",
-    "März",
-    "April",
-    "Mai",
-    "Juni",
-    "Juli",
-    "August",
-    "September",
-    "Oktober",
-    "November",
-    "Dezember"
+    "1 Januar",
+    "2 Februar",
+    "3 März",
+    "4 April",
+    "5 Mai",
+    "6 Juni",
+    "7 Juli",
+    "8 August",
+    "9 September",
+    "10 Oktober",
+    "11 November",
+    "12 Dezember"
 ];
 
 const today = new Date();
